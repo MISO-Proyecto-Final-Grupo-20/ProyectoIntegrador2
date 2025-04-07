37dceac383c33a7ba8d4c1f14a2beffa9c69a7a9
Camilo Barreto <fc.barreto@uniandes.edu.co>
2025-04-06 20:45:42 -0500

    feat(configuración): actualizar configuración del proyecto y agregar nuevo APK
    - Se actualizó 'misc.xml' para incluir 'ExternalStorageConfigurationManager' y establecer 'languageLevel' en 'JDK_21'.
    - Se agregó 'android:usesCleartextTraffic' en 'AndroidManifest.xml'.
    - Se introdujo un nuevo archivo binario 'storeflow.apk'.
    - Se modificó 'capacitor.config.ts' para habilitar el plugin 'CapacitorHttp'.

--- UI/apps/mobile/capacitor.config.ts
+++ UI/apps/mobile/capacitor.config.ts
@@ -4,6 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.storeflow',
   appName: 'StoreFlow',
   webDir: '../../dist/apps/mobile/browser',
+  plugins: {
+    'CapacitorHttp': {
+      'enabled': true
+    }
+  }
 };
 
 export default config;
