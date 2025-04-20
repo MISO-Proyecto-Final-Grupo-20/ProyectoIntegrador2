2a53ff9ee7ac172046c4da53251724aa19c0dadf
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2025-04-20 17:50:28 -0500

    Crear microservicio de ventas sin implementación definitiva del pedido

---
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/EndPoints/PedidosEndPoints.cs
@@ -0,0 +1,27 @@
+﻿using MiniValidation;
+using StoreFlow.Ventas.API.Servicios;
+
+namespace StoreFlow.Ventas.API.EndPoints;
+
+public static class PedidosEndPoints
+{
+    public static void MapCrearPedidoEndPont(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/pedido", async (CrearPedidoCommand crearPedido, IPedidoService pedidoService) =>
+        {
+            if (!MiniValidator.TryValidate(crearPedido, out var errors))
+                return Results.ValidationProblem(errors);
+
+            Resultado<CrearPedidoResponse> resultado = await pedidoService.CrearPedidoAsync(crearPedido);
+
+            if (!resultado.EsExitoso)
+                return Results.BadRequest(resultado.Error);
+
+            return Results.Accepted();
+        });
+    }
+}
+
+public record CrearPedidoResponse();
+
+public record CrearPedidoCommand();
--- Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/Program.cs
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/Program.cs
@@ -1,41 +1,85 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using StoreFlow.Ventas.API.Datos;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING_VENTAS");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("La variable de entorno 'CONNECTION_STRING_VENTAS' no está definida.");
+}
+
+var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
+if (string.IsNullOrEmpty(jwtSecret))
+{
+    throw new InvalidOperationException("La variable de entorno 'JWT_SECRET' no está definida.");
+}
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            RoleClaimType = ClaimTypes.Role,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+        };
+    });
+
+
+builder.Services.AddAuthorization(opciones =>
+{
+    opciones.AddPolicy("SoloUsuariosCcp", policy =>
+        policy.RequireRole("UsuarioCcp"));
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddDbContext<VentasDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<VentasDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-}
+
+[ExcludeFromCodeCoverage]
+public partial class Program { }
