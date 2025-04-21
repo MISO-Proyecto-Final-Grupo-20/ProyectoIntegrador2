12dc84a567b3deaa43ac05cefc791dbe70ce660c
Augusto Romero Arango <a.romeroa23@uniandes.edu.co>
2025-04-20 22:58:57 -0500

    Feature/crear pedido cliente/microservicio inventarios (#51)

---
+++ Backend/StoreFlow.Inventarios/StoreFlow.Inventarios.API/Datos/InventariosDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreFlow.Inventarios.API.Datos;
+
+public class InventariosDbContext(DbContextOptions<InventariosDbContext> options) : DbContext(options)
+{
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
