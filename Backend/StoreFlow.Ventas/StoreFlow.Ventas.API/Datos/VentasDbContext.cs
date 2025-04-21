83c644371b568966b6311b78d34f728477331d8a
Augusto Romero Arango <a.romeroa23@uniandes.edu.co>
2025-04-20 19:47:26 -0500

    Feature/crear pedido cliente/micorservicio ventas (#50)
    * Registro de los proyectos de api y tests
    
    * Crear microservicio de ventas sin implementación definitiva del pedido
    
    * Configuración del nginx y k8s para ventas
    
    * .env para docker compose
    
    * Corrección del docker file de ventas
    
    * Ajustes al ci y cd para ventas
    
    * Ajustes en el k8s en el registro de ventas
    
    * Preparación de la clase de pruebas para probar que se esté generando la cobertura en el pipeline

---
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/Datos/VentasDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreFlow.Ventas.API.Datos;
+
+public class VentasDbContext(DbContextOptions<VentasDbContext>options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+         base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/ErrorDeNegocio.cs
@@ -0,0 +1,7 @@
+﻿namespace StoreFlow.Ventas.API;
+
+public abstract class ErrorDeNegocio
+{
+    public abstract string Mensaje { get; }
+    public virtual string Categoria => "ERROR_NEGOCIO";
+}
