177a4f07a0ffeef77c07a4ff9aa6247ce9673299
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2025-05-03 17:22:08 -0500

    Añadir pruebas unitarias para la funcionalidad de guardar pedidos en la base de datos

---
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.Tests/VentasRepositorioTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreFlow.Compartidos.Core.Mensajes.CreacionPedido.Ventas;
+using StoreFlow.Ventas.API.Datos;
+using StoreFlow.Ventas.API.Entidades;
+
+namespace StoreFlow.Ventas.Tests;
+
+public class VentasRepositorioTests
+{
+    
+    [Fact]
+    public async Task GuardarPedidoAsync_DeberiaGuardarPedidoEnLaBaseDeDatos()
+    {
+        var options = new DbContextOptionsBuilder<VentasDbContext>()
+            .UseInMemoryDatabase(databaseName: "VentasTestDb")
+            .Options;
+
+        await using (var context = new VentasDbContext(options))
+        {
+            var pedido = new Pedido(1, new DateTime(2025, 4, 27),
+            [
+                    new ProductoPedido(101, 2, 50, false),
+                    new ProductoPedido(102, 1, 100, false)
+            ]);
+
+            await context.GuardarPedidoAsync(pedido);
+
+            var pedidoGuardado = await context.Pedidos
+                .Include(p => p.ProductosPedidos)
+                .FirstOrDefaultAsync(p => p.Id == 1);
+
+            Assert.NotNull(pedidoGuardado);
+            Assert.Equal(1, pedidoGuardado.IdCliente);
+            Assert.Equal(1, pedidoGuardado.Id);
+            Assert.Equal(new DateTime(2025, 4, 27), pedidoGuardado.FechaCreacion);
+            
+            Assert.Equal(2, pedidoGuardado.ProductosPedidos.Count);
+            Assert.Contains(pedidoGuardado.ProductosPedidos, pp => pp is {IdProducto: 101, Cantidad: 2, Precio: 50, IdPedido:1, TieneInventario: false});
+            Assert.Contains(pedidoGuardado.ProductosPedidos, pp => pp is {IdProducto: 102, Cantidad: 1, Precio: 100, IdPedido:1, TieneInventario: false});
+            
+        }
+    }
+    
+    [Fact]
+    public async Task GuardarPedidoAsync_DeberiaGuardarDesdeSolicitudPedidoEnLaBaseDeDatos()
+    {
+        var options = new DbContextOptionsBuilder<VentasDbContext>()
+            .UseInMemoryDatabase(databaseName: "VentasTestDb")
+            .Options;
+
+        await using (var context = new VentasDbContext(options))
+        {
+            var solicitudPedido = new SolicitudDePedido(1, new DateTime(2025, 4, 27),
+                [
+                    new ProductoSolicitado(101, 2, 50, false),
+                    new ProductoSolicitado(102, 1, 100, false)
+                ]);
+            var pedido = new Pedido(solicitudPedido);
+            
+            await context.GuardarPedidoAsync(pedido);
+
+            var pedidoGuardado = await context.Pedidos
+                .Include(p => p.ProductosPedidos)
+                .FirstOrDefaultAsync(p => p.Id == 1);
+
+            Assert.NotNull(pedidoGuardado);
+            Assert.Equal(1, pedidoGuardado.IdCliente);
+            Assert.Equal(1, pedidoGuardado.Id);
+            Assert.Equal(new DateTime(2025, 4, 27), pedidoGuardado.FechaCreacion);
+            
+            Assert.Equal(2, pedidoGuardado.ProductosPedidos.Count);
+            Assert.Contains(pedidoGuardado.ProductosPedidos, pp => pp is {IdProducto: 101, Cantidad: 2, Precio: 50, IdPedido:1, TieneInventario: false});
+            Assert.Contains(pedidoGuardado.ProductosPedidos, pp => pp is {IdProducto: 102, Cantidad: 1, Precio: 100, IdPedido:1, TieneInventario: false});
+            
+        }
+    }
+    
+}
