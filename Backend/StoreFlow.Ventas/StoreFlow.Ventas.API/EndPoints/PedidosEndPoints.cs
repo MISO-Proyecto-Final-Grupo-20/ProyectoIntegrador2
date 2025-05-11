72e2d6a421f45b7ef8f7ecc451c9c18fda3ae2bb
Camilo Barreto <fc.barreto@uniandes.edu.co>
2025-05-11 13:01:43 -0500

    Refactoriza el manejo de tokens JWT en los endpoints de pedidos, extrayendo la lógica de recuperación del ID de usuario a una clase utilitaria.

--- Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/EndPoints/PedidosEndPoints.cs
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/EndPoints/PedidosEndPoints.cs
@@ -11,70 +11,60 @@ public static class PedidosEndPoints
 {
     public static void MapCrearPedidoEndPont(this IEndpointRouteBuilder app)
     {
-        app.MapPost("/pedidos", async (HttpContext httpContext, ProductoPedidoRequest[] crearPedido, IPublishEndpoint publishEndpoint, IDateTimeProvider dateTimeProvider) =>
+        app.MapPost("/pedidos",
+            async (HttpContext httpContext, ProductoPedidoRequest[] crearPedido, IPublishEndpoint publishEndpoint,
+                IDateTimeProvider dateTimeProvider) =>
+            {
+                var idUsuario = UtilidadesEndPoints.RecuperarIdUsuarioToken(httpContext);
+
+                var pedidoRequest = new CrearPedidoRequest(crearPedido);
+
+                var solicitud = pedidoRequest.CrearSolicitud(idUsuario, dateTimeProvider.UtcNow, null);
+                var procesarPedido = new ProcesarPedido(Guid.CreateVersion7(), solicitud);
+
+                await publishEndpoint.Publish(procesarPedido);
+
+                return Results.Accepted(null, idUsuario);
+            }).RequireAuthorization("Cliente");
+
+        app.MapPost("/pedidos/{idCliente:int}", async (HttpContext httpContext, int idCliente,
+            ProductoPedidoRequest[] crearPedido, IPublishEndpoint publishEndpoint,
+            IDateTimeProvider dateTimeProvider) =>
         {
-            var token = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            var idUsuario = UtilidadesEndPoints.RecuperarIdUsuarioToken(httpContext);
+
             var pedidoRequest = new CrearPedidoRequest(crearPedido);
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-            var idUsuario = int.Parse(jwtToken.Claims.First(c => c.Type == "idUsuario").Value);
-            
-            var solicitud = pedidoRequest.CrearSolicitud(idUsuario, dateTimeProvider.UtcNow, null);
-            var procesarPedido = new ProcesarPedido(Guid.CreateVersion7(), solicitud);
-            
-            await publishEndpoint.Publish(procesarPedido);
-            
-            return Results.Accepted(null, idUsuario);
-        }).RequireAuthorization("Cliente");
-        
-        app.MapPost("/pedidos/{idCliente:int}", async (HttpContext httpContext, int idCliente,  ProductoPedidoRequest[] crearPedido, IPublishEndpoint publishEndpoint, IDateTimeProvider dateTimeProvider) =>
-        {
-            var token = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-            var pedidoRequest = new CrearPedidoRequest(crearPedido);
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-            var idUsuario = int.Parse(jwtToken.Claims.First(c => c.Type == "idUsuario").Value);
-            
             var solicitud = pedidoRequest.CrearSolicitud(idCliente, dateTimeProvider.UtcNow, idUsuario);
             var procesarPedido = new ProcesarPedido(Guid.CreateVersion7(), solicitud);
-            
+
             await publishEndpoint.Publish(procesarPedido);
-            
+
             return Results.Accepted(null, idUsuario);
         }).RequireAuthorization("Vendedor");
-        
+
         app.MapGet("/pedidos/pendientes", async (HttpContext httpContext, VentasDbContext ventasDbContext) =>
         {
-            var token = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-            var idUsuario = int.Parse(jwtToken.Claims.First(c => c.Type == "idUsuario").Value);
-            
+            var idUsuario = UtilidadesEndPoints.RecuperarIdUsuarioToken(httpContext);
+
             var pedidos = await ventasDbContext.ObtenerPedidosAsync(idUsuario);
-            
+
             return Results.Ok(pedidos);
         }).RequireAuthorization("Cliente");
-        
-        app.MapGet("/pedidos/pendientes/{idCliente:int}", async (HttpContext httpContext, int idCliente, VentasDbContext ventasDbContext) =>
-        {
-            var token = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-            var idVendedor = int.Parse(jwtToken.Claims.First(c => c.Type == "idUsuario").Value);
-            
-            var pedidos = await ventasDbContext.ObtenerPedidosAsync(idCliente, idVendedor);
-            
-            return Results.Ok(pedidos);
-        }).RequireAuthorization("Vendedor");
+
+        app.MapGet("/pedidos/pendientes/{idCliente:int}",
+            async (HttpContext httpContext, int idCliente, VentasDbContext ventasDbContext) =>
+            {
+                var idVendedor = UtilidadesEndPoints.RecuperarIdUsuarioToken(httpContext);
+
+                var pedidos = await ventasDbContext.ObtenerPedidosAsync(idCliente, idVendedor);
+
+                return Results.Ok(pedidos);
+            }).RequireAuthorization("Vendedor");
 
         app.MapPost("/consultaInformes", async (ReporteVentasRequest request, VentasDbContext ventasDbContext) =>
         {
-            ReporteVentasResponse[] report = await ventasDbContext.ObtenerReporteVentasAsync(request);
+            var report = await ventasDbContext.ObtenerReporteVentasAsync(request);
             return Results.Ok(report);
         }).RequireAuthorization("SoloUsuariosCcp");
-
-
     }
-
-    
 }
---
+++ Backend/StoreFlow.Ventas/StoreFlow.Ventas.API/EndPoints/UtilidadesEndPoints.cs
@@ -0,0 +1,15 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace StoreFlow.Ventas.API.EndPoints;
+
+public static class UtilidadesEndPoints
+{
+    public static int RecuperarIdUsuarioToken(HttpContext httpContext)
+    {
+        var token = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+        var handler = new JwtSecurityTokenHandler();
+        var jwtToken = handler.ReadJwtToken(token);
+        var idUsuario = int.Parse(jwtToken.Claims.First(c => c.Type == "idUsuario").Value);
+        return idUsuario;
+    }
+}
